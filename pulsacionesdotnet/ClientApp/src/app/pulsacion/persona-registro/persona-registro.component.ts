ffb59a7b0abd9e22c2fc94fc7b5abaa0b5399d89
HJRumbo <hernandojosern@gmail.com>
2020-04-30 11:16:41 -0500

    Se actualizo el JQuery

--- pulsacionesdotnet/ClientApp/src/app/pulsacion/persona-registro/persona-registro.component.ts
+++ pulsacionesdotnet/ClientApp/src/app/pulsacion/persona-registro/persona-registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from './../models/persona';
-import { PersonaService } from './../../services/persona.service'
+import { PersonaService } from './../../services/persona.service';
+import { FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-persona-registro',
@@ -9,16 +10,54 @@ import { PersonaService } from './../../services/persona.service'
 })
 export class PersonaRegistroComponent implements OnInit {
 
+  formGroup: FormGroup;
   persona:  Persona;
-  constructor(private personaService: PersonaService) { }
+  constructor(private personaService: PersonaService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.persona= new  Persona();
+    this.buildForm();
+  }
+
+  private buildForm(){
+    this.persona = new Persona();
+    this.persona.identificacion = '';
+    this.persona.nombre = '';
+    this.persona.edad = 0;
+    this.persona.sexo = '';
+    this.persona.pulsaciones = 0;
+
+    this.formGroup = this.formBuilder.group({
+      identificacion: [this.persona.identificacion, Validators.required],
+      nombre: [this.persona.nombre, Validators.required],
+      sexo: [this.persona.sexo, [Validators.required, this.validaSexo]],
+      edad: [this.persona.edad, [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  private validaSexo(control: AbstractControl){
+    const sexo = control.value;
+    if(sexo.toLocaleUpperCase() !== 'M' && sexo.toLocaleUpperCase() !== 'F'){
+      return { validSexo: true, messageSexo: 'Sexo no valido'};
+    }else{
+      return null;
+    }
+  }
+
+  get control(){
+
+    return this.formGroup.controls;
+  }
+
+  onSubmit(){
+    if(this.formGroup.invalid){
+      return;
+    }
+    this.add();
   }
 
   add() {
 
-    this.persona.pulsaciones = 0;
+    this.persona = this.formGroup.value;
     this.personaService.post(this.persona).subscribe(p => {
       if (p != null) {
         alert('Persona creada!');
